8763712ee78afc6443f2e47142eae2d953c7581a
timnanevo <tnevo@macalester.edu>
2024-10-02 20:07:42 -0500

    list of cells is moving and growing

--- src/cellabsorption/CellSimulation.java
+++ src/cellabsorption/CellSimulation.java
@@ -6,13 +6,15 @@ import edu.macalester.graphics.Point;
 
 import java.awt.Color;
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 @SuppressWarnings("SameParameterValue")
 public class CellSimulation{
 
     private CanvasWindow canvas;
     private Random rand = new Random();
-    private Cell cell;
+    private List<Cell> cells;
 
 
     public static void main(String[] args) {
@@ -28,23 +30,26 @@ public class CellSimulation{
         //noinspection InfiniteLoopStatement
         while (true) {
             Point canvasCenter = new Point(canvas.getWidth() / 2.0, canvas.getHeight() / 2.0);
-            cell.moveAround(canvasCenter);
-            cell.grow(0.02);
-
+            for (Cell cell : cells){
+                cell.moveAround(canvasCenter);
+                cell.grow(0.02);
+            }
             canvas.draw();
             canvas.pause(10);
         }
     }
 
     private void populateCells() {
-        double size = rand.nextInt(5) + 2;
-        cell = new Cell(
-            rand.nextDouble() * (canvas.getWidth() - size),
-            rand.nextDouble() * (canvas.getWidth() - size),
-            size,
-            Color.getHSBColor(rand.nextFloat(), rand.nextFloat() * 0.5f + 0.1f, 1));
-            
-            canvas.add(cell.getShape());
+        cells = new ArrayList<>();
+            for(int i=0; i <200; i++){
+                double size = rand.nextInt(5) + 2;
+                double x = rand.nextDouble() * (canvas.getWidth() - size);
+                double y = rand.nextDouble() * (canvas.getWidth() - size);
+                Color color = Color.getHSBColor(rand.nextFloat(), rand.nextFloat() * 0.5f + 0.1f, 1);
+                Cell cell = new Cell(x, y, size, color);
+                canvas.add(cell.getShape());
+                cells.add(cell);
+            }
     }
 
 
